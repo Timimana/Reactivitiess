ed11bb8d0127c36fb8bff10c3372a3b8488c19ab
Timimana <timothyimana@gmail.com>
2025-01-08 13:36:01 +0000

    End of section 10

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -2,18 +2,19 @@ using MediatR;
 using Persistence;
 using Domain;
 using System;
+using Application.Core;
 
 namespace Application.Activities
 {
     public class Details
     {
-        public class Query : IRequest<Activity>
+        public class Query : IRequest<Result<Activity>>
         {
             public Guid Id { get; set; }
 
         }
 
-        public class Handler : IRequestHandler<Query, Activity>
+        public class Handler : IRequestHandler<Query, Result<Activity>>
         {
         private readonly DataContext _context;
             public Handler(DataContext context)
@@ -21,10 +22,14 @@ namespace Application.Activities
                  _context = context;
             }
 
-            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Activities.FindAsync(request.Id);
-            }
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                return Result<Activity>.Success(activity);
+
+  
         }
     }
+}
 }
--- client-app/src/app/stores/stores.ts
+++ client-app/src/app/stores/stores.ts
@@ -1,12 +1,16 @@
-import { createContext, useContext } from "react";
 import ActivityStore from "./activityStores";
+import { createContext, useContext } from "react";
+import CommonStore from "./commonStore.ts";
+
 
 interface Store {
     activityStore: ActivityStore
+    commonStore: CommonStore;
 }
 
 export const store: Store = {
-    activityStore: new ActivityStore()
+    activityStore: new ActivityStore(),
+    commonStore: new CommonStore()
 }
 
 export const StoreContext = createContext(store);
